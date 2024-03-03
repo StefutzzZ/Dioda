d805d78a84630134e75895b995a953826c6432a5
StefutzzZ <162043515+StefutzzZ@users.noreply.github.com>
2024-03-03 18:41:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openarticles(){
+    window.location.href='index.html';
+}
+
+function opencontact(){
+    window.location.href='contact.html';
+}
+
+function openreviews(){
+    window.location.href='reviews.html';
+}
